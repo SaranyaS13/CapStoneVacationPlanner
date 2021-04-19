82c903212be8adf6023ba6f37508e625c39df18e
SaranyaS13 <82345946+SaranyaS13@users.noreply.github.com>
2021-04-18 22:36:57 -0400

    Add files via upload

---
+++ CapStone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CapStone_Vacation_Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Welcome to CapStone Vacation Planner ***");
+            String name = "Saranya";
+            Console.WriteLine($"Welcome {name}.");
+            Console.WriteLine("Can you please choose your desired destination : 1.Hawaii 2.Brazil");
+            String Destination = Console.ReadLine();
+            Console.WriteLine("Your choice is " + Destination);
+            Console.ReadLine();
+        }
+    }
+}
