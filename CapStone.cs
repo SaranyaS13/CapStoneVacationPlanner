bd98877664b62164191953a41be802eb573706ba
SaranyaS13 <82345946+SaranyaS13@users.noreply.github.com>
2021-04-18 22:38:14 -0400

    Added Comment

--- CapStone.cs
+++ CapStone.cs
@@ -6,8 +6,10 @@ namespace CapStone_Vacation_Planner
     {
         static void Main(string[] args)
         {
+            //Declaration of variables
             Console.WriteLine("*** Welcome to CapStone Vacation Planner ***");
             String name = "Saranya";
+            //Greeting and showing the available Destination options
             Console.WriteLine($"Welcome {name}.");
             Console.WriteLine("Can you please choose your desired destination : 1.Hawaii 2.Brazil");
             String Destination = Console.ReadLine();
