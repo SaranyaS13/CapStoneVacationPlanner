ba631fd9e68a4fff339bef6cc0984811489db06c
SaranyaS13 <82345946+SaranyaS13@users.noreply.github.com>
2021-05-08 17:13:36 -0400

    Update CapStone.cs

--- CapStone.cs
+++ CapStone.cs
@@ -6,14 +6,18 @@ namespace CapStone_Vacation_Planner
     {
         static void Main(string[] args)
         {
-            //Declaration of variables
-            Console.WriteLine("*** Welcome to CapStone Vacation Planner ***");
-            String name = "Saranya";
-            //Greeting and showing the available Destination options
+            Console.WriteLine("*** Welcome to CapStone Vacation Planner 12***");
+            Console.WriteLine("May I have your name?");
+            String name = Console.ReadLine();
             Console.WriteLine($"Welcome {name}.");
             Console.WriteLine("Can you please choose your desired destination : 1.Hawaii 2.Brazil");
             String Destination = Console.ReadLine();
-            Console.WriteLine("Your choice is " + Destination);
+            Console.WriteLine("How many days you would like to stay in {0}", Destination);
+            int daysofstay = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the spending money you would like to bring?");
+            Double spendingCash = Double.Parse(Console.ReadLine());
+            Double Cashperday = spendingCash / daysofstay;
+            Console.WriteLine("Hey {0} , You are going to {1} for {2} days with {3} for spending money. You can spend {4} per day", name, Destination, daysofstay, spendingCash, Cashperday);
             Console.ReadLine();
         }
     }
